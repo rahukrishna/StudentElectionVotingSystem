2aa2fe4ac7c8649128d117da6df40d23c513a2e3
pkrahul0 <pkrahul0@b2b.regn.net>
2026-06-03 12:01:11 +0530

    Add BvbValanchery GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/StudentElectionVotingSystest/',
   plugins: [
     react({
       include: /\.[jt]sx?$/
